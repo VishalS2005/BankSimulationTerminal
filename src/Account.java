252ff8376a09d909bbf260f67ba57831dba164f0
wekantakabotdis <chenyiningchris@gmail.com>
2025-02-03 10:56:48 -0500

    Account Class Created

--- src/Account.java
+++ src/Account.java
@@ -3,11 +3,36 @@ public class Account implements Comparable<Account> {
     private AccountNumber number;
     private Profile holder;
     private double balance;
-    public void withdraw(double amount) {} //to update the balance
-    public void deposit(double amount) {} //to update the balance
+
+
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    } //to update the balance
+    public void deposit(double amount) {
+        this.balance += amount;
+    } //to update the balance
 
     @Override
     public int compareTo(Account o) {
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Account#[" + number + "] Holder[" + holder + "] Balance[" + balance + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Account) {
+            Account a = (Account) obj;
+            return a.number == this.number;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
